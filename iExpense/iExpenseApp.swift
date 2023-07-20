cf837db4c5ca187e53f164843139ba583939b0cb
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-07-19 19:14:31 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Isaac Dyess on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
